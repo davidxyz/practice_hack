d9fe9514a1b90d33998164bf0b869eda55bf5a63
davidxyz <hunpatind2@yahoo.com>
2016-01-23 11:34:00 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import jibo from 'jibo';
+let {Status, factory} = jibo.bt;
+
+
+function start() {
+    let root = factory.create('../behaviors/fam_idle');
+    root.start();
+    let intervalId = setInterval(() => {
+        if (root.status !== Status.IN_PROGRESS) {
+            clearInterval(intervalId);
+        }
+        else {
+            root.update();
+        }
+    }, 33);
+}
+
+jibo.init().then(() => {
+    require('./behaviors/debug-behavior');
+    let eyeElement = document.getElementById('eye');
+    jibo.visualize.createRobotRenderer(eyeElement, jibo.visualize.DisplayType.EYE);
+    start();
+}).catch(e => {
+    console.error(e);
+});
