15b806ba7edeca32daf7d6d367c8b1606988e381
davidxyz <hunpatind2@yahoo.com>
2016-01-23 16:16:52 -0600

    updates

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let {Status, factory} = jibo.bt;
 
 
 function start() {
-    let root = factory.create('../behaviors/main_luke');
+    let root = factory.create('../behaviors/luke_fallen');
     root.start();
     let intervalId = setInterval(() => {
         if (root.status !== Status.IN_PROGRESS) {
